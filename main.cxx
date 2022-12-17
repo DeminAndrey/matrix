f4e25090320076b5c09ef28ecc9055b4c3afd129
Andrey Demin <a.demin@innopolis.ru>
2022-12-17 15:54:39 +0300

    matrix

--- main.cxx
+++ main.cxx
@@ -9,13 +9,36 @@ void test_from_otus() {
     Matrix<int, -1> m;         // бесконечная матрица int заполнена значениями -1
     assert(m.size() == 0);     // все ячейки свободны
     auto a = m[0][0];
-    //assert(a == -1);
+    assert(a == -1);
     assert(m.size() == 0);
     m[100][100] = 314;
-    //assert(m[100][100] == 314);
+    assert(m[100][100] == 314);
     assert(m.size() == 1);
     std::cout << __func__ << ": OK" << std::endl;
 }
+
+void test_chess() {
+    const int value = -1;
+    int size = 10;
+    Matrix <int, value> m;
+    int value_iter = 0;
+    for(int i=0; i<size; ++i) {
+        for(int j= i%2; j<size; j+=2) {
+            m[i][j] = value;
+            ++value_iter;
+        }
+    }
+    std::cout <<"value iterations: " << value_iter << std::endl;
+    std::cout <<"size: " << m.size() << std::endl;
+
+    for(int i=0; i<size; ++i) {
+        for(int j= 0; j<size; ++j) {
+            std::cout << m[i][j]  << " " ;
+        }
+        std::cout << std::endl;
+    }
+}
+
 void test1() {
     Matrix <int, 0> m;
     for (auto i = 0 ; i < 10 ; i++){
@@ -52,31 +75,9 @@ void test3() {
   std::cout << m[100][100] << std::endl;
   std::cout << m[10000][100000] << std::endl;
 }
-void test_chess() {
-    const int value = -1;
-    int size = 10;
-    Matrix <int, value> m;
-    int value_iter = 0;
-    for(int i=0; i<size; ++i) {
-        for(int j= i%2; j<size; j+=2) {
-            m[i][j] = value;
-            ++value_iter;
-        }
-    }
-    std::cout <<"value iterations: " << value_iter << std::endl;
-    std::cout <<"size: " << m.size() << std::endl;
 
-    for(int i=0; i<size; ++i) {
-        for(int j= 0; j<size; ++j) {
-            std::cout << m[i][j]  << " " ;
-        }
-        std::cout << std::endl;
-    }
-}
 int main() {
-    test_from_otus();
-    test1();
-    test2();
-    test3();
-    test_chess();
+  test1();
+  test2();
+  test3();
 }
--- phys_matrix.h
+++ phys_matrix.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <map>
 #include <utility>
 
@@ -7,9 +8,9 @@ template<typename T, T value>
 class PhysicaMatrix {
   using matrix_position = std::pair<size_t, size_t>;
 
-  static constexpr T _default = value;
-  std::map<matrix_position, T> matrix;
-  matrix_position pair;
+  static const T defaultValue = value;
+  std::map<matrix_position, T> m_matrix;
+  matrix_position m_current;
 
 public:
   PhysicaMatrix() = default;
@@ -18,7 +19,9 @@ public:
   void setRow(size_t row);
 
   PhysicaMatrix& operator[](size_t pos);
-  auto& operator=(const T& other);
+  PhysicaMatrix& operator=(const T& other);
+
+  bool operator==(T val) const;
   operator auto();
 
   auto begin();
@@ -27,45 +30,56 @@ public:
 
 template<typename T, T value>
 size_t PhysicaMatrix<T, value>::size() const {
-  return matrix.size();
-}
-
-template<typename T, T value>
-PhysicaMatrix<T, value>& PhysicaMatrix<T, value>::operator[](size_t pos) {
-  pair.second = pos;
-  return *this;
-}
-
-template<typename T, T value>
-auto& PhysicaMatrix<T, value>::operator=(const T& other) {
-  if (other != _default) {
-    matrix[pair] = other;
-  }
-  else {
-    auto it = matrix.find(pair);
-    if ( it != matrix.end())
-      matrix.erase(it->first);
-  }
-  return *this;
+  return m_matrix.size();
 }
 
 template<typename T, T value>
 void PhysicaMatrix<T, value>::setRow(size_t row) {
-  pair.first = row;
+  m_current.first = row;
 }
 
 template<typename T, T value>
-auto PhysicaMatrix<T, value>::begin() {
-  return matrix.begin();
-};
+PhysicaMatrix<T, value>& PhysicaMatrix<T, value>::operator[](size_t pos) {
+  m_current.second = pos;
+  return *this;
+}
 
 template<typename T, T value>
-auto PhysicaMatrix<T, value>::end() {
-  return matrix.end();
+PhysicaMatrix<T, value>& PhysicaMatrix<T, value>::operator=(const T& other) {
+  if (other != defaultValue) {
+    m_matrix[m_current] = other;
+  }
+  else {
+    auto it = m_matrix.find(m_current);
+    if ( it != m_matrix.end())
+      m_matrix.erase(it->first);
+  }
+  return *this;
+}
+
+template<typename T, T value>
+bool PhysicaMatrix<T, value>::operator==(T val) const {
+  auto it = m_matrix.find(m_current);
+  if (it != m_matrix.end()) {
+    return (it->second == val) ? true : false;
+  }
+  else {
+    return (val == defaultValue) ? true : false;
+  }
 }
 
 template<typename T, T value>
 PhysicaMatrix<T, value>::operator auto() {
-  auto it = matrix.find(pair);
-  return (it != matrix.end()) ? it->second : value;
+  auto it = m_matrix.find(m_current);
+  return (it != m_matrix.end()) ? it->second : value;
+}
+
+template<typename T, T value>
+auto PhysicaMatrix<T, value>::begin() {
+  return m_matrix.begin();
+};
+
+template<typename T, T value>
+auto PhysicaMatrix<T, value>::end() {
+  return m_matrix.end();
 }
